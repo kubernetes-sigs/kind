f200cbc6e45e0a3cc4c376a0de85c93fc18c9824
Benjamin Elder <bentheelder@google.com>
2018-11-17 00:20:03 -0800

    clarify main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// This package is a stub main wrapping kind.Main()
+// This package is a stub main wrapping cmd/kind.Main()
 package main
 
 import (
