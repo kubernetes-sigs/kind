4249aea82a32943a0231703f5034b821db7aa431
Vasilis Remmas <vasremm@gmail.com>
2018-11-27 14:12:42 +0100

    Check if userns-remap is enabled

--- pkg/cluster/nodes/create.go
+++ pkg/cluster/nodes/create.go
@@ -50,34 +50,39 @@ func CreateControlPlaneNode(name, image, clusterLabel string) (handle *Node, por
 	if err != nil {
 		return nil, 0, errors.Wrap(err, "failed to get port for API server")
 	}
+	runArgs := []string{
+		"-d", // run the container detached
+		// running containers in a container requires privileged
+		// NOTE: we could try to replicate this with --cap-add, and use less
+		// privileges, but this flag also changes some mounts that are necessary
+		// including some ones docker would otherwise do by default.
+		// for now this is what we want. in the future we may revisit this.
+		"--privileged",
+		"--security-opt", "seccomp=unconfined", // also ignore seccomp
+		"--tmpfs", "/tmp", // various things depend on working /tmp
+		"--tmpfs", "/run", // systemd wants a writable /run
+		// some k8s things want /lib/modules
+		"-v", "/lib/modules:/lib/modules:ro",
+		"--hostname", name, // make hostname match container name
+		"--name", name, // ... and set the container name
+		// label the node with the cluster ID
+		"--label", clusterLabel,
+		// publish selected port for the API server
+		"--expose", fmt.Sprintf("%d", port),
+		"-p", fmt.Sprintf("%d:%d", port, port),
+		// explicitly set the entrypoint
+		"--entrypoint=/usr/local/bin/entrypoint",
+	}
+
+	if docker.UsernsRemap() {
+		// We need this argument in order to make this command work
+		// in systems that have userns-remap enabled on the docker daemon
+		runArgs = append(runArgs, "--userns=host")
+	}
+
 	id, err := docker.Run(
 		image,
-		[]string{
-			"-d", // run the container detached
-			// We need this argument in order to make this command work
-			// in systems that have userns-remap enabled on the docker daemon
-			"--userns=host",
-			// running containers in a container requires privileged
-			// NOTE: we could try to replicate this with --cap-add, and use less
-			// privileges, but this flag also changes some mounts that are necessary
-			// including some ones docker would otherwise do by default.
-			// for now this is what we want. in the future we may revisit this.
-			"--privileged",
-			"--security-opt", "seccomp=unconfined", // also ignore seccomp
-			"--tmpfs", "/tmp", // various things depend on working /tmp
-			"--tmpfs", "/run", // systemd wants a writable /run
-			// some k8s things want /lib/modules
-			"-v", "/lib/modules:/lib/modules:ro",
-			"--hostname", name, // make hostname match container name
-			"--name", name, // ... and set the container name
-			// label the node with the cluster ID
-			"--label", clusterLabel,
-			// publish selected port for the API server
-			"--expose", fmt.Sprintf("%d", port),
-			"-p", fmt.Sprintf("%d:%d", port, port),
-			// explicitly set the entrypoint
-			"--entrypoint=/usr/local/bin/entrypoint",
-		},
+		runArgs,
 		[]string{
 			// explicitly pass the entrypoint argument
 			"/sbin/init",
