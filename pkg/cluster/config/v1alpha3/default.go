07d72c80e9f8fed851b7ada5bb469ffab0e970cb
Benjamin Elder <bentheelder@google.com>
2019-03-08 19:24:57 -0800

    add defaults and registration to v1alpha3

---
+++ pkg/cluster/config/v1alpha3/default.go
@@ -0,0 +1,50 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha3
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"sigs.k8s.io/kind/pkg/cluster/config/defaults"
+)
+
+func addDefaultingFuncs(scheme *runtime.Scheme) error {
+	return RegisterDefaults(scheme)
+}
+
+// SetDefaults_Cluster sets uninitialized fields to their default value.
+func SetDefaults_Cluster(obj *Cluster) {
+	if len(obj.Nodes) == 0 {
+		obj.Nodes = []Node{
+			{
+				Image: defaults.Image,
+				Role:  ControlPlaneRole,
+			},
+		}
+	}
+}
+
+// SetDefaults_Node sets uninitialized fields to their default value.
+func SetDefaults_Node(obj *Node) {
+	if obj.Image == "" {
+		obj.Image = defaults.Image
+	}
+
+	if obj.Role == "" {
+		obj.Role = ControlPlaneRole
+	}
+}
---
+++ pkg/cluster/config/v1alpha3/register.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha3
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+// GroupName is the group name used in this package.
+const GroupName = "kind.sigs.k8s.io"
+
+var (
+	// SchemeGroupVersion is group version used to register these objects.
+	SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1alpha3"}
+	// SchemeBuilder is a type to collect functions that add data to an API
+	// object through a scheme.
+	SchemeBuilder      runtime.SchemeBuilder
+	localSchemeBuilder = &SchemeBuilder
+	// AddToScheme applies all the stored functions in the localSchemeBuilder
+	// to the scheme.
+	AddToScheme = localSchemeBuilder.AddToScheme
+)
+
+func init() {
+	// We only register manually written functions here. The registration of the
+	// generated functions takes place in the generated files. The separation
+	// makes the code compile even when the generated files are missing.
+	localSchemeBuilder.Register(addKnownTypes, addDefaultingFuncs)
+}
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Cluster{},
+	)
+
+	return nil
+}
