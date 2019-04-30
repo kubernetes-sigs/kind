7d1c501f2838c383594818fee554840c7be33b10
Benjamin Elder <bentheelder@google.com>
2019-04-30 01:13:36 -0700

    add support for bazel + < v1.12

--- pkg/container/docker/archive.go
+++ pkg/container/docker/archive.go
@@ -32,7 +32,7 @@ import (
 // GetArchiveTags obtains a list of "repo:tag" docker image tags from a
 // given docker image archive (tarball) path
 // compatible with all known specs:
-// https://github.com/moby/moby/blob/master/image/spec/v1.0.md
+// https://github.com/moby/moby/blob/master/image/spec/v1.md
 // https://github.com/moby/moby/blob/master/image/spec/v1.1.md
 // https://github.com/moby/moby/blob/master/image/spec/v1.2.md
 func GetArchiveTags(path string) ([]string, error) {
@@ -61,10 +61,12 @@ func GetArchiveTags(path string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	var repoTags map[string]map[string]string
-	if err := json.Unmarshal(b, &repoTags); err != nil {
+	// parse
+	repoTags, err := parseRepositories(b)
+	if err != nil {
 		return nil, err
 	}
+	// convert to tags in the docker CLI sense
 	res := []string{}
 	for repo, tags := range repoTags {
 		for tag := range tags {
@@ -72,4 +74,66 @@ func GetArchiveTags(path string) ([]string, error) {
 		}
 	}
 	return res, nil
+}
+
+// ArchiveRepositories represents repository:tag:ref
+//
+// https://github.com/moby/moby/blob/master/image/spec/v1.md
+// https://github.com/moby/moby/blob/master/image/spec/v1.1.md
+// https://github.com/moby/moby/blob/master/image/spec/v1.2.md
+type ArchiveRepositories map[string]map[string]string
+
+// EditArchiveRepositories applies edit to reader's repositories file
+func EditArchiveRepositories(reader io.Reader, writer io.Writer, edit func(ArchiveRepositories) ArchiveRepositories) error {
+	tarReader := tar.NewReader(reader)
+	tarWriter := tar.NewWriter(writer)
+	// iterate all entries in the tarball
+	for {
+		// read an entry
+		hdr, err := tarReader.Next()
+		if err == io.EOF {
+			return tarWriter.Close()
+		} else if err != nil {
+			return err
+		}
+		b, err := ioutil.ReadAll(tarReader)
+		if err != nil {
+			return err
+		}
+
+		// parse the tags if we found the repositories file
+		if hdr.Name == "repositories" {
+			tags, err := parseRepositories(b)
+			if err != nil {
+				return err
+			}
+			// run the user's edit function
+			edit(tags)
+			// marshal back the tags for writing
+			b, err = json.Marshal(tags)
+			if err != nil {
+				return err
+			}
+			hdr.Size = int64(len(b))
+		}
+
+		// write to the output tarball
+		if err := tarWriter.WriteHeader(hdr); err != nil {
+			return err
+		}
+		if len(b) > 0 {
+			if _, err := tarWriter.Write(b); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+// returns repository:tag:ref
+func parseRepositories(data []byte) (ArchiveRepositories, error) {
+	var repoTags ArchiveRepositories
+	if err := json.Unmarshal(data, &repoTags); err != nil {
+		return nil, err
+	}
+	return repoTags, nil
 }
