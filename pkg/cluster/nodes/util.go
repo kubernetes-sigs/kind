9ca5380eb342c6f55516c7c47a3b1633133a5b29
Jintao Zhang <zhangjintao9020@gmail.com>
2019-05-06 23:42:55 +0800

    fix loopback IP address regex
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

--- pkg/cluster/nodes/util.go
+++ pkg/cluster/nodes/util.go
@@ -56,7 +56,7 @@ func addResolve(node *Node) error {
 	}
 
 	// filter the loopback addresses
-	re := regexp.MustCompile("(?m)[\r\n]+^.*127.0.0.1.*$")
+	re := regexp.MustCompile("(?m)[\r\n]+^.*((127.([0-9]{1,3}.){2}[0-9]{1,3})|(::1)).*$")
 	content := re.ReplaceAllString(string(resolv), "")
 
 	err = node.WriteFile("/kind/resolv.conf", content)
