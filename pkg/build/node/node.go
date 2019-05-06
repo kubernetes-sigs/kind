ae7114239152de91d1bbd07021f3e3c277af386a
Benjamin Elder <bentheelder@google.com>
2019-05-06 14:05:31 -0700

    bump base to v20190506-d0ac573

--- pkg/build/node/node.go
+++ pkg/build/node/node.go
@@ -43,7 +43,7 @@ import (
 const DefaultImage = "kindest/node:latest"
 
 // DefaultBaseImage is the default base image used
-const DefaultBaseImage = "kindest/base:v20190505-9ba015b"
+const DefaultBaseImage = "kindest/base:v20190506-d0ac573"
 
 // DefaultMode is the default kubernetes build mode for the built image
 // see pkg/build/kube.Bits
