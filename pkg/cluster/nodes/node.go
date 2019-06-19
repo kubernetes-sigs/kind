632f4a01c2fbe59e2375c92a6d4db748c82be562
Amit Watve <amwat@google.com>
2019-06-18 18:01:10 -0700

    Add docker network to no_proxy.

--- pkg/cluster/nodes/node.go
+++ pkg/cluster/nodes/node.go
@@ -33,6 +33,14 @@ import (
 	"sigs.k8s.io/kind/pkg/exec"
 )
 
+const (
+	// Docker default bridge network is named "bridge" (https://docs.docker.com/network/bridge/#use-the-default-bridge-network)
+	defaultNetwork = "bridge"
+	httpProxy      = "HTTP_PROXY"
+	httpsProxy     = "HTTPS_PROXY"
+	noProxy        = "NO_PROXY"
+)
+
 // Node represents a handle to a kind node
 // This struct must be created by one of: CreateControlPlane
 // It should not be manually instantiated
@@ -257,7 +265,7 @@ type proxyDetails struct {
 // getProxyDetails returns a struct with the host environment proxy settings
 // that should be passed to the nodes
 func getProxyDetails() proxyDetails {
-	var proxyEnvs = []string{"HTTP_PROXY", "HTTPS_PROXY", "NO_PROXY"}
+	var proxyEnvs = []string{httpProxy, httpsProxy, noProxy}
 	var val string
 	var details proxyDetails
 	details.Envs = make(map[string]string)
@@ -273,5 +281,23 @@ func getProxyDetails() proxyDetails {
 			}
 		}
 	}
+
+	// Specifically add the docker network subnets to NO_PROXY
+	subnets, err := getSubnets(defaultNetwork)
+	if err == nil {
+		details.Envs[noProxy] = strings.Join(append(subnets, details.Envs[noProxy]), ",")
+		details.Envs[strings.ToLower(noProxy)] = strings.Join(append(subnets, details.Envs[strings.ToLower(noProxy)]), ",")
+	}
+
 	return details
+}
+
+// getSubnets returns a slice of subnets for a specified network
+func getSubnets(networkName string) ([]string, error) {
+	format := `{{range (index (index . "IPAM") "Config")}}{{index . "Subnet"}} {{end}}`
+	lines, err := docker.NetworkInspect([]string{networkName}, format)
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(lines[0], " "), nil
 }
