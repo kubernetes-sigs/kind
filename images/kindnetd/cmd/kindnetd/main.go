4a52255f0c3f358ddbd946e48bd69e05c1771f7b
Benjamin Elder <bentheelder@google.com>
2019-07-26 00:07:34 -0700

    Kindnetd 0.6.0 with dual-stack support

--- images/kindnetd/cmd/kindnetd/main.go
+++ images/kindnetd/cmd/kindnetd/main.go
@@ -19,15 +19,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"os"
+	"strings"
 	"time"
+
+	"k8s.io/apimachinery/pkg/util/sets"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/klog"
-	"k8s.io/utils/net"
+	utilsnet "k8s.io/utils/net"
 )
 
 // kindnetd is a simple networking daemon to complete kind's CNI implementation
@@ -41,6 +46,18 @@ import (
 // - CNI_CONFIG_TEMPLATE: the cni .conflist template, run with {{ .PodCIDR }}
 
 // TODO: improve logging & error handling
+
+// IPFamily defines kindnet networking operating model
+type IPFamily string
+
+const (
+	// IPv4Family sets IPFamily to ipv4
+	IPv4Family IPFamily = "ipv4"
+	// IPv6Family sets IPFamily to ipv6
+	IPv6Family IPFamily = "ipv6"
+	// DualStackFamily sets ClusterIPFamily to dualstack
+	DualStackFamily IPFamily = "DualStack"
+)
 
 func main() {
 	// enable logging
@@ -59,7 +76,6 @@ func main() {
 	}
 
 	// obtain the host and pod ip addresses
-	// if both ips are different we are not using the host network
 	hostIP, podIP := os.Getenv("HOST_IP"), os.Getenv("POD_IP")
 	klog.Infof("hostIP = %s\npodIP = %s\n", hostIP, podIP)
 	if hostIP != podIP {
@@ -75,20 +91,51 @@ func main() {
 	}
 
 	// enforce ip masquerade rules
-	// TODO: dual stack...?
-	masqAgent, err := NewIPMasqAgent(net.IsIPv6String(hostIP), []string{os.Getenv("POD_SUBNET")})
-	if err != nil {
-		panic(err.Error())
+	noMaskIPv4Subnets, noMaskIPv6Subnets := getNoMasqueradeSubnets(clientset)
+	// detect the cluster IP family based on the Cluster CIDR akka PodSubnet
+	var ipFamily IPFamily
+	if len(noMaskIPv4Subnets) > 0 && len(noMaskIPv6Subnets) > 0 {
+		ipFamily = DualStackFamily
+	} else if len(noMaskIPv6Subnets) > 0 {
+		ipFamily = IPv6Family
+	} else if len(noMaskIPv4Subnets) > 0 {
+		ipFamily = IPv4Family
+	} else {
+		panic(fmt.Sprint("Cluster CIDR is not defined"))
 	}
-	// run the masqAgent and panic if is not able to install the rules to no masquerade the pod to pod traffic
-	go func() {
-		if err := masqAgent.SyncRulesForever(time.Second * 60); err != nil {
+	klog.Infof("kindnetd IP family: %q", ipFamily)
+
+	// create an ipMasqAgent for IPv4
+	if len(noMaskIPv4Subnets) > 0 {
+		klog.Infof("noMask IPv4 subnets: %v", noMaskIPv4Subnets)
+		masqAgentIPv4, err := NewIPMasqAgent(false, noMaskIPv4Subnets)
+		if err != nil {
 			panic(err.Error())
 		}
-	}()
+		go func() {
+			if err := masqAgentIPv4.SyncRulesForever(time.Second * 60); err != nil {
+				panic(err)
+			}
+		}()
+	}
+
+	// create an ipMasqAgent for IPv6
+	if len(noMaskIPv6Subnets) > 0 {
+		klog.Infof("noMask IPv6 subnets: %v", noMaskIPv6Subnets)
+		masqAgentIPv6, err := NewIPMasqAgent(true, noMaskIPv6Subnets)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		go func() {
+			if err := masqAgentIPv6.SyncRulesForever(time.Second * 60); err != nil {
+				panic(err)
+			}
+		}()
+	}
 
 	// setup nodes reconcile function, closes over arguments
-	reconcileNodes := makeNodesReconciler(cniConfigWriter, hostIP)
+	reconcileNodes := makeNodesReconciler(cniConfigWriter, hostIP, ipFamily, clientset)
 
 	// main control loop
 	for {
@@ -127,43 +174,76 @@ func main() {
 }
 
 // nodeNodesReconciler returns a reconciliation func for nodes
-func makeNodesReconciler(cniConfig *CNIConfigWriter, hostIP string) func(*corev1.NodeList) error {
+func makeNodesReconciler(cniConfig *CNIConfigWriter, hostIP string, ipFamily IPFamily, clientset *kubernetes.Clientset) func(*corev1.NodeList) error {
 	// reconciles a node
 	reconcileNode := func(node corev1.Node) error {
-		// first get this node's IP
-		nodeIP := internalIP(node)
-		if nodeIP == "" {
-			klog.Infof("Node %v has no Internal IP, ignoring\n", node.Name)
-			return nil
-		}
-
-		// don't do anything unless there is a PodCIDR
-		podCIDR := node.Spec.PodCIDR
-		if podCIDR == "" {
-			klog.Infof("Node %v has no CIDR, ignoring\n", node.Name)
-			return nil
-		}
-
+		// first get this node's IPs
+		// we don't support more than one IP address per IP family for simplification
+		nodeIPs := internalIPs(node)
+		klog.Infof("Handling node with IPs: %v\n", nodeIPs)
 		// This is our node. We don't need to add routes, but we might need to
-		// update the cni config.
-		if nodeIP == hostIP {
-			klog.Infof("handling current node\n")
+		// update the cni config and "annotate" our external IPs
+		if nodeIPs.Has(hostIP) {
+			klog.Info("handling current node\n")
 			// compute the current cni config inputs
 			if err := cniConfig.Write(
 				ComputeCNIConfigInputs(node),
 			); err != nil {
 				return err
 			}
+			if ipFamily == DualStackFamily {
+				// update external IPs node annotations until #42125 is fixed
+				// TODO: https://github.com/kubernetes/kubernetes/issues/42125
+				// in KIND we know that the outer interface is eth0 to find the external ips
+				// but we can be smarter and find the interface based on the HOST_IP
+				hostExternalIPs := getHostExternalIPs(hostIP)
+				// update the node
+				klog.Infof("Update node with IPs: %v\n", hostExternalIPs)
+				patchString := fmt.Sprintf(`{"metadata": {"annotations": {"kind.x-k8s.io.kindnet/addresses": "%s"}}}`, hostExternalIPs)
+				patchBytes := []byte(patchString)
+				if _, err := clientset.CoreV1().Nodes().Patch(node.Name, types.MergePatchType, patchBytes); err != nil {
+					return fmt.Errorf("failed to patch node: %v", err)
+				}
+			}
 			// we're done handling this node
 			return nil
 		}
 
-		klog.Infof("Handling node with IP: %s\n", nodeIP)
-		klog.Infof("Node %v has CIDR %s \n", node.Name, podCIDR)
-		if err := syncRoute(nodeIP, podCIDR); err != nil {
-			return err
+		// This is another node. Add routes to the POD subnets in the other nodes
+		// don't do anything unless there is a PodCIDR
+		var podCIDRs []string
+		if ipFamily == DualStackFamily {
+			podCIDRs = node.Spec.PodCIDRs
+		} else {
+			podCIDRs = []string{node.Spec.PodCIDR}
+		}
+		if len(podCIDRs) == 0 {
+			fmt.Printf("Node %v has no CIDR, ignoring\n", node.Name)
+			return nil
+		}
+		klog.Infof("Node %v has CIDR %s \n", node.Name, podCIDRs)
+		podCIDRsv4, podCIDRsv6 := splitCIDRs(podCIDRs)
+
+		// obtain the PodCIDR gateway
+		var nodeIPv4, nodeIPv6 string
+		for _, ip := range nodeIPs.List() {
+			if utilsnet.IsIPv6String(ip) {
+				nodeIPv6 = ip
+			} else {
+				nodeIPv4 = ip
+			}
 		}
 
+		if nodeIPv4 != "" && len(podCIDRsv4) > 0 {
+			if err := syncRoute(nodeIPv4, podCIDRsv4); err != nil {
+				return err
+			}
+		}
+		if nodeIPv6 != "" && len(podCIDRsv6) > 0 {
+			if err := syncRoute(nodeIPv6, podCIDRsv6); err != nil {
+				return err
+			}
+		}
 		return nil
 	}
 
@@ -178,12 +258,87 @@ func makeNodesReconciler(cniConfig *CNIConfigWriter, hostIP string) func(*corev1
 	}
 }
 
-// internalIP returns the internalIP address for node
-func internalIP(node corev1.Node) string {
+// internalIPs returns the internal IP address for node
+func internalIPs(node corev1.Node) sets.String {
+	ips := sets.NewString()
+	// check the node.Status.Addresses
 	for _, address := range node.Status.Addresses {
 		if address.Type == "InternalIP" {
-			return address.Address
+			ips.Insert(address.Address)
+		}
+	}
+	// check the node.annotations.Internal.Addresses
+	for _, address := range strings.Split(node.Annotations["kind.x-k8s.io.kindnet/addresses"], ",") {
+		ips.Insert(address)
+	}
+	return ips
+}
+
+// getHostExternalIPs return a comma separated list with the
+// node external IP addresses
+func getHostExternalIPs(ip string) string {
+	return getInterfaceIPs(interfaceByAddress(ip))
+}
+
+// interfaceByAddress return the name of the interface
+// that hosts a given IP address
+func interfaceByAddress(address string) string {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		panic(err.Error())
+	}
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			panic(err.Error())
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPAddr:
+				ip = v.IP
+			case *net.IPNet:
+				ip = v.IP
+			default:
+				continue
+			}
+			if ip.String() == address {
+				return i.Name
+			}
+
 		}
 	}
 	return ""
+}
+
+// getInterfaceIPs returns an array with all the global addresses
+// of the interfaces passed as a parameter
+func getInterfaceIPs(ifazName string) string {
+	var ips string
+
+	ifaz, err := net.InterfaceByName(ifazName)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	addrs, err := ifaz.Addrs()
+	if err != nil {
+		panic(err.Error())
+	}
+	for _, addr := range addrs {
+		var ip net.IP
+		switch v := addr.(type) {
+		case *net.IPAddr:
+			ip = v.IP
+		case *net.IPNet:
+			ip = v.IP
+		default:
+			continue
+		}
+		if ip.IsGlobalUnicast() {
+			ips = ips + "," + ip.String()
+		}
+
+	}
+	return strings.Trim(ips, ",")
 }
--- images/kindnetd/go.mod
+++ images/kindnetd/go.mod
@@ -3,27 +3,24 @@ module sigs.k8s.io/kind/images/kindnetd
 go 1.13
 
 require (
-	github.com/coreos/go-iptables v0.4.3
-	github.com/gogo/protobuf v1.2.1 // indirect
-	github.com/golang/protobuf v1.3.1 // indirect
-	github.com/google/gofuzz v1.0.0 // indirect
-	github.com/googleapis/gnostic v0.2.0 // indirect
-	github.com/json-iterator/go v1.1.6 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/pkg/errors v0.8.1
+	github.com/coreos/go-iptables v0.4.5
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/google/go-cmp v0.3.1 // indirect
+	github.com/googleapis/gnostic v0.3.1 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/vishvananda/netlink v1.0.0
-	github.com/vishvananda/netns v0.0.0-20180720170159-13995c7128cc // indirect
-	golang.org/x/crypto v0.0.0-20190506204251-e1dfcc566284 // indirect
-	golang.org/x/net v0.0.0-20190509222800-a4d6f7feada5 // indirect
-	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a // indirect
-	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
-	gopkg.in/inf.v0 v0.9.1 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
-	k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
-	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
-	k8s.io/client-go v11.0.0+incompatible
-	k8s.io/klog v0.3.0
-	k8s.io/utils v0.0.0-20190506122338-8fab8cb257d5
-	sigs.k8s.io/yaml v1.1.0 // indirect
+	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
+	golang.org/x/crypto v0.0.0-20200115085410-6d4e4cb37c7d // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/sys v0.0.0-20200113162924-86b910548bc1 // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
+	k8s.io/api v0.17.1
+	k8s.io/apimachinery v0.17.1
+	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
+	k8s.io/klog v1.0.0
+	k8s.io/utils v0.0.0-20200109141947-94aeca20bf09
 )
