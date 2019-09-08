d6ed25f466f2050e679b691574d198479bbb2d4a
Benjamin Elder <bentheelder@google.com>
2019-09-08 11:58:35 -0700

    fix spinner Writer should not restart if not already started

--- pkg/internal/util/cli/spinner.go
+++ pkg/internal/util/cli/spinner.go
@@ -45,13 +45,15 @@ var spinnerFrames = []string{
 // Spinner is a simple and efficient CLI loading spinner used by kind
 // It is simplistic and assumes that the line length will not change.
 type Spinner struct {
-	stop   chan struct{}
-	ticker *time.Ticker
-	writer io.Writer
-	mu     *sync.Mutex
-	// protected by mu
-	prefix string
-	suffix string
+	stop    chan struct{} // signals writer goroutine to stop from Stop()
+	stopped chan struct{} // signals Stop() that the writer goroutine stopped
+	mu      *sync.Mutex   // protects the mutable bits
+	// below are protected by mu
+	running bool
+	writer  io.Writer
+	ticker  *time.Ticker // signals that it is time to write a frame
+	prefix  string
+	suffix  string
 }
 
 // spinner implements writer
@@ -60,35 +62,49 @@ var _ io.Writer = &Spinner{}
 // NewSpinner initializes and returns a new Spinner that will write to w
 func NewSpinner(w io.Writer) *Spinner {
 	return &Spinner{
-		stop:   make(chan struct{}, 1),
-		ticker: time.NewTicker(time.Millisecond * 100),
-		mu:     &sync.Mutex{},
-		writer: w,
+		stop:    make(chan struct{}, 1),
+		stopped: make(chan struct{}, 1),
+		mu:      &sync.Mutex{},
+		writer:  w,
 	}
 }
 
 // SetPrefix sets the prefix to print before the spinner
 func (s *Spinner) SetPrefix(prefix string) {
 	s.mu.Lock()
+	defer s.mu.Unlock()
 	s.prefix = prefix
-	s.mu.Unlock()
 }
 
 // SetSuffix sets the suffix to print after the spinner
 func (s *Spinner) SetSuffix(suffix string) {
 	s.mu.Lock()
+	defer s.mu.Unlock()
 	s.suffix = suffix
-	s.mu.Unlock()
 }
 
 // Start starts the spinner running
 func (s *Spinner) Start() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	// don't start if we've already started, also flag that we've started
+	if s.running {
+		return
+	}
+	s.running = true
+	// start / create a frame ticker
+	s.ticker = time.NewTicker(time.Millisecond * 100)
+	// spin in the background
 	go func() {
+		// write frames forever (until signaled to stop)
 		for {
 			for _, frame := range spinnerFrames {
 				select {
+				// prefer stopping, select this signal first
 				case <-s.stop:
-					return
+					s.stopped <- struct{}{} // signal that we stopped
+					return                  // ... and stop
+				// otherwise continue and write one frame
 				case <-s.ticker.C:
 					func() {
 						s.mu.Lock()
@@ -103,17 +119,35 @@ func (s *Spinner) Start() {
 
 // Stop signals the spinner to stop
 func (s *Spinner) Stop() {
+	s.mu.Lock()
+	// if there's nothing to stop, return early
+	if !s.running {
+		s.mu.Unlock()
+		return
+	}
+	// otherwise: mark not running, send the stop signal and release the ticker
 	s.stop <- struct{}{}
+	s.ticker.Stop()
+	s.running = false
+	// unlock after sending the signal to avoid deadlock
+	s.mu.Unlock()
+	// and wait for the reply
+	<-s.stopped
 }
 
 // Write implements io.Writer, interrupting the spinner and writing to
 // the inner writer
 func (s *Spinner) Write(p []byte) (n int, err error) {
-	s.Stop()
+	// lock first, so nothing else can start writing until we are done
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	// it the spinner is not running, just write directly
+	if !s.running {
+		return s.writer.Write(p)
+	}
+	// otherwise: we will rewrite the line first
 	if _, err := s.writer.Write([]byte("\r")); err != nil {
 		return 0, err
 	}
-	n, err = s.writer.Write(p)
-	s.Start()
-	return n, err
+	return s.writer.Write(p)
 }
