74286bce5806ce682aad894da9dbc825a1c57ff3
Benjamin Elder <bentheelder@google.com>
2019-09-11 13:24:09 -0700

    doc concurrent package

--- pkg/util/concurrent/concurrent.go
+++ pkg/util/concurrent/concurrent.go
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package concurrent provides utilities for concurrent execution
+// TODO(bentheelder): this should be internal
 package concurrent
 
 import (
