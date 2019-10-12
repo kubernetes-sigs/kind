3ba6e43de2a17d441e4abf84773a9a15f8bbadde
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2019-10-12 17:29:05 +0200

    Use bash instead of sh

--- hack/ci/e2e-k8s.sh
+++ hack/ci/e2e-k8s.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Copyright 2018 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -187,7 +187,7 @@ main() {
   time create_cluster
 
   # run e2e tests
-  echo "IFO: Running e2e tests"
+  echo "INFO: Running e2e tests"
   time run_tests
 }
 
