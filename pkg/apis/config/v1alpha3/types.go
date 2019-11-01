7bf1d6078884fa37de39a4a0079da9ea73ee4a4f
Benjamin Elder <bentheelder@google.com>
2019-10-31 22:06:00 -0700

    drop json from config

--- pkg/apis/config/v1alpha3/types.go
+++ pkg/apis/config/v1alpha3/types.go
@@ -18,28 +18,28 @@ package v1alpha3
 
 // Cluster contains kind cluster configuration
 type Cluster struct {
-	TypeMeta `yaml:",inline" json:",inline"`
+	TypeMeta `yaml:",inline"`
 
 	// Nodes contains the list of nodes defined in the `kind` Cluster
 	// If unset this will default to a single control-plane node
 	// Note that if more than one control plane is specified, an external
 	// control plane load balancer will be provisioned implicitly
-	Nodes []Node `yaml:"nodes,omitempty" json:"nodes,omitempty"`
+	Nodes []Node `yaml:"nodes,omitempty"`
 
 	/* Advanced fields */
 
 	// Networking contains cluster wide network settings
-	Networking Networking `yaml:"networking,omitempty" json:"networking,omitempty"`
+	Networking Networking `yaml:"networking,omitempty"`
 
 	// KubeadmConfigPatches are applied to the generated kubeadm config as
 	// strategic merge patches to `kustomize build` internally
 	// https://github.com/kubernetes/community/blob/master/contributors/devel/strategic-merge-patch.md
 	// This should be an inline yaml blob-string
-	KubeadmConfigPatches []string `yaml:"kubeadmConfigPatches,omitempty" json:"kubeadmConfigPatches,omitempty"`
+	KubeadmConfigPatches []string `yaml:"kubeadmConfigPatches,omitempty"`
 
 	// KubeadmConfigPatchesJSON6902 are applied to the generated kubeadm config
 	// as patchesJson6902 to `kustomize build`
-	KubeadmConfigPatchesJSON6902 []PatchJSON6902 `yaml:"kubeadmConfigPatchesJson6902,omitempty" json:"kubeadmConfigPatchesJson6902,omitempty"`
+	KubeadmConfigPatchesJSON6902 []PatchJSON6902 `yaml:"kubeadmConfigPatchesJson6902,omitempty"`
 }
 
 // TypeMeta partially copies apimachinery/pkg/apis/meta/v1.TypeMeta
@@ -57,22 +57,22 @@ type Node struct {
 	// created by kind
 	//
 	// Defaults to "control-plane"
-	Role NodeRole `yaml:"role,omitempty" json:"role,omitempty"`
+	Role NodeRole `yaml:"role,omitempty"`
 
 	// Image is the node image to use when creating this node
 	// If unset a default image will be used, see defaults.Image
-	Image string `yaml:"image,omitempty" json:"image,omitempty"`
+	Image string `yaml:"image,omitempty"`
 
 	/* Advanced fields */
 
 	// TODO: cri-like types should be inline instead
 	// ExtraMounts describes additional mount points for the node container
 	// These may be used to bind a hostPath
-	ExtraMounts []Mount `yaml:"extraMounts,omitempty" json:"extraMounts,omitempty"`
+	ExtraMounts []Mount `yaml:"extraMounts,omitempty"`
 
 	// ExtraPortMappings describes additional port mappings for the node container
 	// binded to a host Port
-	ExtraPortMappings []PortMapping `yaml:"extraPortMappings,omitempty" json:"extraPortMappings,omitempty"`
+	ExtraPortMappings []PortMapping `yaml:"extraPortMappings,omitempty"`
 }
 
 // NodeRole defines possible role for nodes in a Kubernetes cluster managed by `kind`
@@ -91,24 +91,24 @@ const (
 // Networking contains cluster wide network settings
 type Networking struct {
 	// IPFamily is the network cluster model, currently it can be ipv4 or ipv6
-	IPFamily ClusterIPFamily `yaml:"ipFamily,omitempty" json:"ipFamily,omitempty"`
+	IPFamily ClusterIPFamily `yaml:"ipFamily,omitempty"`
 	// APIServerPort is the listen port on the host for the Kubernetes API Server
 	// Defaults to a random port on the host
-	APIServerPort int32 `yaml:"apiServerPort,omitempty" json:"apiServerPort,omitempty"`
+	APIServerPort int32 `yaml:"apiServerPort,omitempty"`
 	// APIServerAddress is the listen address on the host for the Kubernetes
 	// API Server. This should be an IP address.
 	//
 	// Defaults to 127.0.0.1
-	APIServerAddress string `yaml:"apiServerAddress,omitempty" json:"apiServerAddress,omitempty"`
+	APIServerAddress string `yaml:"apiServerAddress,omitempty"`
 	// PodSubnet is the CIDR used for pod IPs
 	// kind will select a default if unspecified
-	PodSubnet string `yaml:"podSubnet,omitempty" json:"podSubnet,omitempty"`
+	PodSubnet string `yaml:"podSubnet,omitempty"`
 	// ServiceSubnet is the CIDR used for services VIPs
 	// kind will select a default if unspecified for IPv6
-	ServiceSubnet string `yaml:"serviceSubnet,omitempty" json:"serviceSubnet,omitempty"`
+	ServiceSubnet string `yaml:"serviceSubnet,omitempty"`
 	// If DisableDefaultCNI is true, kind will not install the default CNI setup.
 	// Instead the user should install their own CNI after creating the cluster.
-	DisableDefaultCNI bool `yaml:"disableDefaultCNI,omitempty" json:"disableDefaultCNI,omitempty"`
+	DisableDefaultCNI bool `yaml:"disableDefaultCNI,omitempty"`
 }
 
 // ClusterIPFamily defines cluster network IP family
@@ -125,16 +125,16 @@ const (
 // https://tools.ietf.org/html/rfc6902
 type PatchJSON6902 struct {
 	// these fields specify the patch target resource
-	Group   string `yaml:"group" json:"group"`
-	Version string `yaml:"version" json:"version"`
-	Kind    string `yaml:"kind" json:"kind"`
+	Group   string `yaml:"group"`
+	Version string `yaml:"version"`
+	Kind    string `yaml:"kind"`
 	// Name and Namespace are optional
 	// NOTE: technically name is required now, but we default it elsewhere
 	// Third party users of this type / library would need to set it.
-	Name      string `yaml:"name,omitempty" json:"name,omitempty"`
-	Namespace string `yaml:"namespace,omitempty" json:"namespace,omitempty"`
+	Name      string `yaml:"name,omitempty"`
+	Namespace string `yaml:"namespace,omitempty"`
 	// Patch should contain the contents of the json patch as a string
-	Patch string `yaml:"patch" json:"patch"`
+	Patch string `yaml:"patch"`
 }
 
 /*
@@ -157,17 +157,17 @@ https://github.com/kubernetes/kubernetes/blob/063e7ff358fdc8b0916e6f39beedc0d025
 // Propagation may be one of: None, HostToContainer, Bidirectional
 type Mount struct {
 	// Path of the mount within the container.
-	ContainerPath string `yaml:"containerPath,omitempty" json:"containerPath,omitempty"`
+	ContainerPath string `yaml:"containerPath,omitempty"`
 	// Path of the mount on the host. If the hostPath doesn't exist, then runtimes
 	// should report error. If the hostpath is a symbolic link, runtimes should
 	// follow the symlink and mount the real destination to container.
-	HostPath string `yaml:"hostPath,omitempty" json:"hostPath,omitempty"`
+	HostPath string `yaml:"hostPath,omitempty"`
 	// If set, the mount is read-only.
-	Readonly bool `yaml:"readOnly,omitempty" json:"readOnly,omitempty"`
+	Readonly bool `yaml:"readOnly,omitempty"`
 	// If set, the mount needs SELinux relabeling.
-	SelinuxRelabel bool `yaml:"selinuxRelabel,omitempty" json:"selinuxRelabel,omitempty"`
+	SelinuxRelabel bool `yaml:"selinuxRelabel,omitempty"`
 	// Requested propagation mode.
-	Propagation MountPropagation `yaml:"propagation,omitempty" json:"propagation,omitempty"`
+	Propagation MountPropagation `yaml:"propagation,omitempty"`
 }
 
 // PortMapping specifies a host port mapped into a container port.
@@ -178,13 +178,13 @@ type Mount struct {
 //  protocol: TCP
 type PortMapping struct {
 	// Port within the container.
-	ContainerPort int32 `yaml:"containerPort,omitempty" json:"containerPort,omitempty"`
+	ContainerPort int32 `yaml:"containerPort,omitempty"`
 	// Port on the host.
-	HostPort int32 `yaml:"hostPort,omitempty" json:"hostPort,omitempty"`
+	HostPort int32 `yaml:"hostPort,omitempty"`
 	// TODO: add protocol (tcp/udp) and port-ranges
-	ListenAddress string `yaml:"listenAddress,omitempty" json:"listenAddress,omitempty"`
+	ListenAddress string `yaml:"listenAddress,omitempty"`
 	// Protocol (TCP/UDP)
-	Protocol PortMappingProtocol `yaml:"protocol,omitempty" json:"protocol,omitempty"`
+	Protocol PortMappingProtocol `yaml:"protocol,omitempty"`
 }
 
 // MountPropagation represents an "enum" for mount propagation options,
