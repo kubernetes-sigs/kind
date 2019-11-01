709bb412ed0308025fb69633f7cac66dcd89f122
Benjamin Elder <bentheelder@google.com>
2019-10-31 22:02:30 -0700

    tidy provider package

--- pkg/cluster/provider.go
+++ pkg/cluster/provider.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package cluster implements kind kubernetes-in-docker cluster management
 package cluster
 
 import (
