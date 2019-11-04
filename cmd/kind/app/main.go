0d19659ff269bd600a8ac63135bd00f8906f008c
Dario Tranchitella <dario@tranchitella.eu>
2019-11-04 17:51:33 +0100

    Moving logging to Run

--- cmd/kind/app/main.go
+++ cmd/kind/app/main.go
@@ -2,22 +2,24 @@ package app
 
 import (
 	"os"
-	"sigs.k8s.io/kind/pkg/globals"
-
 	"sigs.k8s.io/kind/pkg/cmd/kind"
+	"sigs.k8s.io/kind/pkg/globals"
 )
 
 // Main is the kind main(), it will invoke Run(), if an error is returned
 // it will then call os.Exit
 func Main() {
 	if err := Run(); err != nil {
-		globals.GetLogger().Error(err.Error())
 		os.Exit(1)
 	}
 }
 
 // Run invokes the kind root command, returning the error.
 // See: sigs.k8s.io/kind/pkg/cmd/kind
-func Run() error {
-	return kind.NewCommand().Execute()
+func Run() (err error) {
+	err = kind.NewCommand().Execute()
+	if err != nil {
+		globals.GetLogger().Error(err.Error())
+	}
+	return
 }
