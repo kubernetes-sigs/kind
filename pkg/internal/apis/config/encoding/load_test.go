3a9ca5b6140ca8571b7663d7ee87536927a79289
Roberto J Rojas <robertojrojas@gmail.com>
2019-11-13 16:42:39 -0500

    adds unit test

--- pkg/internal/apis/config/encoding/load_test.go
+++ pkg/internal/apis/config/encoding/load_test.go
@@ -98,6 +98,11 @@ func TestLoadCurrent(t *testing.T) {
 			ExpectError: false,
 		},
 		{
+			TestName:    "v1alpha4 config with workers patches",
+			Path:        "./testdata/v1alpha4/valid-kind-workers-patches.yaml",
+			ExpectError: false,
+		},
+		{
 			TestName:    "v1alpha4 non-existent field",
 			Path:        "./testdata/v1alpha4/invalid-bogus-field.yaml",
 			ExpectError: true,
