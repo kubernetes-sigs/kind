6a5570bdd07a9ab6744778ab62dc85512f149250
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2019-12-02 18:40:39 +0100

    Test kindnetd image with the patch

--- pkg/build/node/cni.go
+++ pkg/build/node/cni.go
@@ -27,7 +27,7 @@ const (
 The default CNI manifest and images are our own tiny kindnet
 */
 
-var defaultCNIImages = []string{"kindest/kindnetd:0.5.4"}
+var defaultCNIImages = []string{"aojea/kindnetd:0.5.4"}
 
 const defaultCNIManifest = `
 # kindnetd networking manifest
@@ -100,7 +100,7 @@ spec:
       serviceAccountName: kindnet
       containers:
       - name: kindnet-cni
-        image: kindest/kindnetd:0.5.4
+        image: aojea/kindnetd:0.5.4
         env:
         - name: HOST_IP
           valueFrom:
