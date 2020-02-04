a59c506c5805ad43ce79f6782e1e380e48cb2ee7
Amit Watve <amwat@google.com>
2020-02-04 13:20:07 -0800

    Fix kind get clusters on podman.

--- pkg/cluster/internal/providers/podman/provider.go
+++ pkg/cluster/internal/providers/podman/provider.go
@@ -92,7 +92,7 @@ func (p *Provider) ListClusters() ([]string, error) {
 		// filter for nodes with the cluster label
 		"--filter", "label="+clusterLabelKey,
 		// format to include the cluster name
-		"--format", fmt.Sprintf(`{{.Label "%s"}}`, clusterLabelKey),
+		"--format", fmt.Sprintf(`{{index .Labels "%s"}}`, clusterLabelKey),
 	)
 	lines, err := exec.OutputLines(cmd)
 	if err != nil {
