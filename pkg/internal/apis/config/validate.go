3665b8f1cf4d1277df2bea0c25b4dd7a0fed9e28
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2020-05-04 18:22:51 +0200

    fix validation

--- pkg/internal/apis/config/validate.go
+++ pkg/internal/apis/config/validate.go
@@ -104,13 +104,13 @@ func (n *Node) Validate() error {
 	}
 
 	// validate node resource constraints
-	if n.Constraints.Cpus.Sign() < 0 {
+	if n.Constraints.Cpus.Sign() != 1 {
 		errs = append(errs, errors.New("invalid number of Cpus"))
 	}
 
 	// minimum memory size is 4m
 	minMemory := resource.MustParse("4m")
-	if n.Constraints.Memory.Sign() != 0 && n.Constraints.Memory.Cmp(minMemory) < 0 {
+	if n.Constraints.Memory.Sign() != 1 && n.Constraints.Memory.Cmp(minMemory) < 0 {
 		errs = append(errs, errors.New("invalid Memory Size (minimum 4m)"))
 	}
 
