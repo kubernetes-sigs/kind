f7e86aacbecfe0f23042e7ceb92b92ba59ac3b44
Marc Villacorta <mvillacorta@newrelic.com>
2020-05-16 17:42:44 +0200

    Fixes kind-with-registry.sh

--- site/static/examples/kind-with-registry.sh
+++ site/static/examples/kind-with-registry.sh
@@ -22,10 +22,10 @@ containerdConfigPatches:
 EOF
 
 # connect the registry to the cluster network
-docker network connect "kind" "${reg_name}"
+docker network connect "kind" "${reg_name}" 2>/dev/null || true
 
 # tell https://tilt.dev to use the registry
 # https://docs.tilt.dev/choosing_clusters.html#discovering-the-registry
 for node in $(kind get nodes); do
-  kubectl annotate node "${node}" "kind.x-k8s.io/registry=localhost:${reg_port}";
+  kubectl annotate node "${node}" "tilt.dev/registry=localhost:${reg_port}"
 done
