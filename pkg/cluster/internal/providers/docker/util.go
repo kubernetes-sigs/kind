5b1ab097e5fe028360b86fec67a2af061997ec82
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2020-05-19 17:49:19 +0200

    verify the provider is not fake
    each provider implements a function to detect if it is available
    Docker has preference to maintain backwards compatibility

--- pkg/cluster/internal/providers/docker/util.go
+++ pkg/cluster/internal/providers/docker/util.go
@@ -22,6 +22,16 @@ import (
 	"sigs.k8s.io/kind/pkg/exec"
 )
 
+// IsAvailable checks if docker is available in the system
+func IsAvailable() bool {
+	cmd := exec.Command("docker", "-v")
+	lines, err := exec.OutputLines(cmd)
+	if err != nil || len(lines) != 1 {
+		return false
+	}
+	return strings.HasPrefix(lines[0], "Docker version")
+}
+
 // usernsRemap checks if userns-remap is enabled in dockerd
 func usernsRemap() bool {
 	cmd := exec.Command("docker", "info", "--format", "'{{json .SecurityOptions}}'")
