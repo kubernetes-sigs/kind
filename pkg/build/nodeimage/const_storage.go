286a6c82bc39f8dbfd0f4a789dbd2f818dd53d70
Benjamin Elder <bentheelder@google.com>
2020-06-17 20:17:28 -0700

    bump debian-base

--- pkg/build/nodeimage/const_storage.go
+++ pkg/build/nodeimage/const_storage.go
@@ -25,7 +25,7 @@ NOTE: we have customized it in the following ways:
 - install as the default storage class
 */
 
-var defaultStorageImages = []string{"rancher/local-path-provisioner:v0.0.12", "k8s.gcr.io/debian-base:v2.0.0"}
+var defaultStorageImages = []string{"rancher/local-path-provisioner:v0.0.12", "us.gcr.io/k8s-artifacts-prod/build-image/debian-base:v2.1.0"}
 
 const defaultStorageManifest = `
 # kind customized https://github.com/rancher/local-path-provisioner manifest
@@ -102,7 +102,7 @@ spec:
         - --debug
         - start
         - --helper-image
-        - k8s.gcr.io/debian-base:v2.0.0
+        - us.gcr.io/k8s-artifacts-prod/build-image/debian-base:v2.1.0
         - --config
         - /etc/config/config.json
         volumeMounts:
