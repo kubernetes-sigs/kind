39419e33c74960ac24ca91a7923b7268b35bfea6
Benjamin Elder <bentheelder@google.com>
2020-07-26 23:11:52 -0700

    fix shellcheck exclude dir

--- hack/verify/shellcheck.sh
+++ hack/verify/shellcheck.sh
@@ -36,7 +36,7 @@ SHELLCHECK_IMAGE="koalaman/shellcheck:v0.7.1"
 all_shell_scripts=()
 while IFS=$'\n' read -r script;
   do git check-ignore -q "$script" || all_shell_scripts+=("$script");
-done < <(grep -irl '#!.*sh' . --exclude-dir={bin\*,.git\*,vendor\*})
+done < <(grep -irl '#!.*sh' . --exclude-dir={./bin\*,./.git\*,./vendor\*})
 
 # common arguments we'll pass to shellcheck
 SHELLCHECK_OPTIONS=(
