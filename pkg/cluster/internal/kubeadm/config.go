9d5a700ed1db9cd858c52c564a69eea0b9545a4a
Benjamin Elder <bentheelder@google.com>
2020-08-24 16:12:21 -0700

    set providerID on nodes

--- pkg/cluster/internal/kubeadm/config.go
+++ pkg/cluster/internal/kubeadm/config.go
@@ -39,21 +39,34 @@ type ConfigData struct {
 	APIBindPort int
 	// The API server external listen IP (which we will port forward)
 	APIServerAddress string
+
+	// this should really be used for the --provider-id flag
+	// ideally cluster config should not depend on the node backend otherwise ...
+	NodeProvider string
+
 	// ControlPlane flag specifies the node belongs to the control plane
 	ControlPlane bool
 	// The main IP address of the node
 	NodeAddress string
+	// The name for the node (not the address)
+	NodeName string
+
 	// The Token for TLS bootstrap
 	Token string
+
 	// KubeProxyMode defines the kube-proxy mode between iptables or ipvs
 	KubeProxyMode string
 	// The subnet used for pods
 	PodSubnet string
 	// The subnet used for services
 	ServiceSubnet string
-	// IPv4 values take precedence over IPv6 by default, if true set IPv6 default values
-	IPv6         bool
+
+	// Kubernetes FeatureGates
 	FeatureGates map[string]bool
+
+	// IPv4 values take precedence over IPv6 by default, if true set IPv6 default values
+	IPv6 bool
+
 	// DerivedConfigData is populated by Derive()
 	// These auto-generated fields are available to Config templates,
 	// but not meant to be set by hand
