8b209c85ca4dfb3e5a7dde73866958c65bbf1f52
StevenSu <suwei007@gmail.com>
2020-10-19 22:03:53 +0800

    add server to pkg/cmd/kind/root.go

--- pkg/cmd/kind/root.go
+++ pkg/cmd/kind/root.go
@@ -31,6 +31,7 @@ import (
 	"sigs.k8s.io/kind/pkg/cmd/kind/export"
 	"sigs.k8s.io/kind/pkg/cmd/kind/get"
 	"sigs.k8s.io/kind/pkg/cmd/kind/load"
+	"sigs.k8s.io/kind/pkg/cmd/kind/server"
 	"sigs.k8s.io/kind/pkg/cmd/kind/version"
 	"sigs.k8s.io/kind/pkg/log"
 )
@@ -87,6 +88,7 @@ func NewCommand(logger log.Logger, streams cmd.IOStreams) *cobra.Command {
 	cmd.AddCommand(get.NewCommand(logger, streams))
 	cmd.AddCommand(version.NewCommand(logger, streams))
 	cmd.AddCommand(load.NewCommand(logger, streams))
+	cmd.AddCommand(server.NewCommand(logger, streams))
 	return cmd
 }
 
