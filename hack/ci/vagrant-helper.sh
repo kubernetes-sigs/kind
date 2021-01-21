1c76d7197b22952454cd2c9415f7b545415aa0f8
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2021-01-21 14:41:20 +0900

    add cgroup2 CI
    Use Fedora 33 as the cgroup2 host.
    
    Fedora is launched as a Vagrant VM.
    
    Related:
    - https://github.com/containerd/containerd/blob/c4bff3d17cd0d92e516e755252e98f92d224cf2f/.github/workflows/ci.yml#L352-L378
    - https://github.com/opencontainers/runc/blob/2adbc66b710f89bea89b6804137c934279a4abff/.github/workflows/test.yml#L91-L122
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

---
+++ hack/ci/vagrant-helper.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Copyright 2021 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit -o nounset -o pipefail
+
+
+: "${KIND_EXPERIMENTAL_PROVIDER:=docker}"
+SSH_CONFIG=".vagrant/ssh-config"
+if [ ! -f "$SSH_CONFIG" ]; then
+  vagrant ssh-config > "$SSH_CONFIG"
+fi
+
+exec ssh -F "$SSH_CONFIG" default sudo KIND_EXPERIMENTAL_PROVIDER="$KIND_EXPERIMENTAL_PROVIDER" "$@"
