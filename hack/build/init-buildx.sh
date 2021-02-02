160fe6b3e4c69cfebe2752dd9cde469d122377a1
duchuan <59048253+duchuanLX@users.noreply.github.com>
2021-02-02 06:40:58 +0000

    Update init-buildx.sh

--- hack/build/init-buildx.sh
+++ hack/build/init-buildx.sh
@@ -21,7 +21,7 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 # We can skip setup if the current builder already has multi-arch
 # AND if it isn't the docker driver, which doesn't work
 current_builder="$(docker buildx inspect)"
-# linux/amd64, linux/arm64, linux/riscv64, linux/ppc64le, linux/s390x, linux/386, linux/arm/v7, linux/arm/v6
+# linux/amd64, linux/arm64, linux/riscv64, linux/ppc64le, linux/s390x, linux/386, linux/arm/v7, linux/arm/v6, linux/mips64le
 if ! grep -q "^Driver: docker$"  <<<"${current_builder}" && \
      grep -q "linux/amd64" <<<"${current_builder}" && \
      grep -q "linux/arm64" <<<"${current_builder}"; then
