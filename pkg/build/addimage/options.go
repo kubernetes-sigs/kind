0e8e10d7c7171bb179f7029b0cbf3de814c1f0a9
Sean McGinnis <sean.mcginnis@gmail.com>
2021-06-02 09:18:09 -0400

    Add ability to include images in custom node build
    This adds a new `kind build add-image` command that provides the ability
    to "preload" container images in a custom node image.
    
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>
    Co-authored-by: Curt Bushko <cbushko@gmail.com>
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

---
+++ pkg/build/addimage/options.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package addimage
+
+import (
+	"sigs.k8s.io/kind/pkg/log"
+)
+
+// Option is a configuration option supplied to Build
+type Option interface {
+	apply(*buildContext) error
+}
+
+type optionAdapter func(*buildContext) error
+
+func (c optionAdapter) apply(o *buildContext) error {
+	return c(o)
+}
+
+// WithImage configures a build to tag the built image with `image`
+func WithImage(image string) Option {
+	return optionAdapter(func(b *buildContext) error {
+		b.image = image
+		return nil
+	})
+}
+
+// WithBaseImage configures a build to use `image` as the base image
+func WithBaseImage(image string) Option {
+	return optionAdapter(func(b *buildContext) error {
+		b.baseImage = image
+		return nil
+	})
+}
+
+// WithAdditionalImages configures a build to add images to the node image
+func WithAdditonalImages(images []string) Option {
+	return optionAdapter(func(b *buildContext) error {
+		b.additionalImages = images
+		return nil
+	})
+}
+
+// WithLogger sets the logger
+func WithLogger(logger log.Logger) Option {
+	return optionAdapter(func(b *buildContext) error {
+		b.logger = logger
+		return nil
+	})
+}
+
+// WithArch sets the architecture to build for
+func WithArch(arch string) Option {
+	return optionAdapter(func(b *buildContext) error {
+		if arch != "" {
+			b.arch = arch
+		}
+		return nil
+	})
+}
