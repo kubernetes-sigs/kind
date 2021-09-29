90097e282af7709ffb345eb0c3fdd1703fea82e5
Peter Valdemar Mørch <peter@morch.com>
2021-09-30 01:49:34 +0200

    examples/kind-with-registry.sh now also works with other than reg_port='5000'
    kind-registry is accessed from inside docker, so
    regardless of what port is used when starting it with:
    
        -p "127.0.0.1:${reg_port}:5000"
    
    It must always be accessed as kind-registry:5000 and not
    kind-registry:${reg_port}

--- site/static/examples/kind-with-registry.sh
+++ site/static/examples/kind-with-registry.sh
@@ -18,7 +18,7 @@ apiVersion: kind.x-k8s.io/v1alpha4
 containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
-    endpoint = ["http://${reg_name}:${reg_port}"]
+    endpoint = ["http://${reg_name}:5000"]
 EOF
 
 # connect the registry to the cluster network
