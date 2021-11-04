626c485bc91aaed4e5bbcc31ad634d9092d902c1
Antonio Ojea <aojea@redhat.com>
2021-11-04 12:41:09 +0100

    bump node image

--- pkg/apis/config/defaults/image.go
+++ pkg/apis/config/defaults/image.go
@@ -18,4 +18,4 @@ limitations under the License.
 package defaults
 
 // Image is the default for the Config.Image field, aka the default node image.
-const Image = "kindest/node:v1.22.2@sha256:f638a08c1f68fe2a99e724ace6df233a546eaf6713019a0b310130a4f91ebe7f"
+const Image = "kindest/node:v1.22.2@sha256:9af3ab3e36fb59890b2fb3a18000930b4792d62d10d2060e0ca701e2c392d487"
