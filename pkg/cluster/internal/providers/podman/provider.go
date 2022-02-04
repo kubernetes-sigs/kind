069242b9654110a0c6a38771475f42b4507d3537
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2022-02-04 22:48:54 +0100

    only mount /dev/fuse for rootless

--- pkg/cluster/internal/providers/podman/provider.go
+++ pkg/cluster/internal/providers/podman/provider.go
@@ -430,8 +430,10 @@ func info(logger log.Logger) (*providers.ProviderInfo, error) {
 		SupportsCPUShares:   cgroupSupportsCPUShares,
 	}
 	if info.Rootless && !v.AtLeast(version.MustParseSemantic("4.0.0")) {
-		logger.Warn("Cgroup controller detection is not implemented for Podman. " +
-			"If you see cgroup-related errors, you might need to set systemd property \"Delegate=yes\", see https://kind.sigs.k8s.io/docs/user/rootless/")
+		if logger != nil {
+			logger.Warn("Cgroup controller detection is not implemented for Podman. " +
+				"If you see cgroup-related errors, you might need to set systemd property \"Delegate=yes\", see https://kind.sigs.k8s.io/docs/user/rootless/")
+		}
 	}
 	return info, nil
 }
