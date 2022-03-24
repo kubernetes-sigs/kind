490387f90f073c221182d027595f7346930d899f
Benjamin Elder <bentheelder@google.com>
2022-03-23 20:08:48 -0700

    bump base

--- pkg/build/nodeimage/defaults.go
+++ pkg/build/nodeimage/defaults.go
@@ -20,4 +20,4 @@ package nodeimage
 const DefaultImage = "kindest/node:latest"
 
 // DefaultBaseImage is the default base image used
-const DefaultBaseImage = "docker.io/kindest/base:v20220322-030aa9e1"
+const DefaultBaseImage = "docker.io/kindest/base:v20220323-56f3ce67"
