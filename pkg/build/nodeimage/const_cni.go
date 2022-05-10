8d010d49691a815cc1e096d1c348f5d87572a885
Benjamin Elder <bentheelder@google.com>
2022-05-10 15:44:20 -0700

    bump kindnetd

--- pkg/build/nodeimage/const_cni.go
+++ pkg/build/nodeimage/const_cni.go
@@ -20,7 +20,7 @@ package nodeimage
 The default CNI manifest and images are our own tiny kindnet
 */
 
-const kindnetdImage = "docker.io/kindest/kindnetd:v20211122-a2c10462"
+const kindnetdImage = "docker.io/kindest/kindnetd:v20220510-4929dd75"
 
 var defaultCNIImages = []string{kindnetdImage}
 
