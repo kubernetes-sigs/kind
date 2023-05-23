d104d336bfead0b23e5a86ef9423ad77188f42d9
Benjamin Elder <bentheelder@google.com>
2023-05-23 10:35:47 -0700

    bump node image with containerd 1.7.1

--- pkg/apis/config/defaults/image.go
+++ pkg/apis/config/defaults/image.go
@@ -18,4 +18,4 @@ limitations under the License.
 package defaults
 
 // Image is the default for the Config.Image field, aka the default node image.
-const Image = "kindest/node:v1.27.2@sha256:dfc3d4e956559d64fa5728f5b000acabf8a007a34c098d9f1efced0f2ecc7d60"
+const Image = "kindest/node:v1.27.2@sha256:f8c4cbfb438b777ad84c2707efee91571294593a871071486369229102411e20"
