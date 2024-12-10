794538f726df2c4d5e0f2d1f3c02a2163168ba87
Benjamin Elder <bentheelder@google.com>
2024-12-09 20:27:03 -0800

    add fallback version detection for tarballs < v1.31

--- pkg/build/nodeimage/internal/kube/builder_remote.go
+++ pkg/build/nodeimage/internal/kube/builder_remote.go
@@ -27,6 +27,7 @@ import (
 	"strings"
 	"time"
 
+	"sigs.k8s.io/kind/pkg/errors"
 	"sigs.k8s.io/kind/pkg/log"
 )
 
@@ -78,8 +79,15 @@ func (b *remoteBuilder) Build() (Bits, error) {
 
 	binDir := filepath.Join(tmpDir, "kubernetes/server/bin")
 	contents, err := os.ReadFile(filepath.Join(tmpDir, "kubernetes/version"))
+	// fallback for Kubernetes < v1.31 which doesn't have the version file
+	// this approach only works for release tags as the format happens to match
+	// for pre-release builds the docker tag is mangled and not valid semver
+	if err != nil && os.IsNotExist(err) {
+		b.logger.Warn("WARNING: Using fallback version detection due to missing version file (This command works best with Kubernetes v1.31+)")
+		contents, err = os.ReadFile(filepath.Join(binDir, "kube-apiserver.docker_tag"))
+	}
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "failed to get version")
 	}
 	sourceVersionRaw := strings.TrimSpace(string(contents))
 	return &bits{
