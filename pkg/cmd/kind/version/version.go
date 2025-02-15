74998a68f224e8c41ce323ec4469d75d4fec6cf8
Benjamin Elder <bentheelder@google.com>
2025-02-14 16:45:04 -0800

    version v0.28.0-alpha

--- pkg/cmd/kind/version/version.go
+++ pkg/cmd/kind/version/version.go
@@ -58,11 +58,11 @@ func DisplayVersion() string {
 }
 
 // versionCore is the core portion of the kind CLI version per Semantic Versioning 2.0.0
-const versionCore = "0.27.0"
+const versionCore = "0.28.0"
 
 // versionPreRelease is the base pre-release portion of the kind CLI version per
 // Semantic Versioning 2.0.0
-var versionPreRelease = ""
+var versionPreRelease = "alpha"
 
 // gitCommitCount count the commits since the last release.
 // It is injected at build time.
