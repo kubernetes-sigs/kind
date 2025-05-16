ea1db0a2d82af4d389a394af7aebe91b6d1d700f
Sean McGinnis <sean.mcginnis@gmail.com>
2025-05-16 17:18:56 -0500

    Update default image to use v1.33.1
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- pkg/apis/config/defaults/image.go
+++ pkg/apis/config/defaults/image.go
@@ -18,4 +18,4 @@ limitations under the License.
 package defaults
 
 // Image is the default for the Config.Image field, aka the default node image.
-const Image = "kindest/node:v1.33.0@sha256:91e9ed777db80279c22d1d1068c091b899b2078506e4a0f797fbf6e397c0b0b2"
+const Image = "kindest/node:v1.33.1@sha256:8d866994839cd096b3590681c55a6fa4a071fdaf33be7b9660e5697d2ed13002"
