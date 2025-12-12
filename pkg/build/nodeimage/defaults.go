8ebd20690a686537fdae9a0e8605c98c8ebd0558
Benjamin Elder <bentheelder@google.com>
2025-12-12 10:45:56 -0800

    bump base image

--- pkg/build/nodeimage/defaults.go
+++ pkg/build/nodeimage/defaults.go
@@ -22,4 +22,4 @@ const DefaultImage = "kindest/node:latest"
 // DefaultBaseImage is the default base image used
 // TODO: come up with a reasonable solution to digest pinning
 // https://github.com/moby/moby/issues/43188
-const DefaultBaseImage = "docker.io/kindest/base:v20250710-v0.29.0-alpha-25-g9ac770cf"
+const DefaultBaseImage = "docker.io/kindest/base:v20251212-v0.29.0-alpha-105-g20ccfc88"
