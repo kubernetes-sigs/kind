2519a3ad14d376dfa5718b48c5a788036f3e3e0b
Benjamin Elder <bentheelder@google.com>
2025-12-19 16:07:06 -0800

    use staging node image to test

--- pkg/apis/config/defaults/image.go
+++ pkg/apis/config/defaults/image.go
@@ -18,4 +18,4 @@ limitations under the License.
 package defaults
 
 // Image is the default for the Config.Image field, aka the default node image.
-const Image = "kindest/node:v1.35.0@sha256:4613778f3cfcd10e615029370f5786704559103cf27bef934597ba562b269661"
+const Image = "gcr.io/k8s-staging-kind/node:v1.35.0@sha256:bca18d834b32618a99c38bdb16ab14ebf02e4c90a0b40574db90ea982bb953e0"
