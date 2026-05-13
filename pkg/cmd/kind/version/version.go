9e6eb3230aa7ae0fae20ea03ef97a925ddccb7f3
kaylaisher <147053990+kaylaisher@users.noreply.github.com>
2026-05-13 12:30:44 +0800

    Fix comment formatting in version.go

--- pkg/cmd/kind/version/version.go
+++ pkg/cmd/kind/version/version.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package version implements the `version` command
+// Package version implements the `version` command.
 package version
 
 import (
