3900b2ee547e1522136c10fc552ab9e97b446a22
Sean McGinnis <sean.mcginnis@gmail.com>
2026-05-29 10:26:26 -0500

    Add copyright header enforcement
    This adopts the CI enforcement done in prow to check and verify source
    files contain the expected copyright headers we require.
    
    Addresses header issues where it was either missing or contained typos.
    
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- site/tools.go
+++ site/tools.go
@@ -2,10 +2,24 @@
 // +build tools
 
 /*
-Package site is used to track binary dependencies with go modules
-https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
-Namely: hugo
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
+
+// Package site is used to track binary dependencies with go modules
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+// Namely: hugo
 package site
 
 import (
